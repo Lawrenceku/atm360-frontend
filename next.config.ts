584503a28be62d8bdeedd649151471366c8d19db
Lawrence <lawrencekunmilola@gmail.com>
2025-11-14 05:58:40 +0100

    hide nextjs bubble

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   // Setting this to false hides the indicator
-  // devIndicators: false,
+  devIndicators: false,
 };
 
 export default nextConfig;
